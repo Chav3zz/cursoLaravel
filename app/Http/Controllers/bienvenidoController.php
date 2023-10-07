e4ba8a1c91087be76c119acb7c8912ac87ba6615
Adrian <adrianchavez3d@gmail.com>
2023-10-07 14:10:10 -0600

    07/10/2023

---
+++ app/Http/Controllers/bienvenidoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class bienvenidoController extends Controller
+{
+    public function bienvenido(){
+        $nombre = "Adrián";
+        $alumnos = ['Ximena', 'Perez', 'Sergio', 'Evelyn', 'Daniella', 'Juan', 'David', 'Kyan', 'Eitan', 'Adrian', 'Leo'];
+        $datos = array( ['nombre' => 'Ximena', 'edad' => 17, 'turno' => 'Vespertino'],
+                        ['nombre' => 'Perez', 'edad' => 20, 'turno' => 'Matutino'],
+                        ['nombre' => 'Sergio', 'edad' => 15, 'turno' => 'Nocturno'],
+                        ['nombre' => 'Evelyn', 'edad' => 16, 'turno' => 'Vespertino'],
+                        ['nombre' => 'Daniella', 'edad' => 18, 'turno' => 'Matutino'],
+                        ['nombre' => 'Juan', 'edad' => 24, 'turno' => 'Matutino'],
+                        ['nombre' => 'David', 'edad' => 28, 'turno' => 'Vespertino']);
+        return view('bienvenido', compact('nombre', 'alumnos', 'datos'));
+    }
+}
